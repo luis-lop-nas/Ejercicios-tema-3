b23116862b2ed7ea77f82da8dcf2e7f0c36b363d
luis-lop-nas <luislopnas@icloud.com>
2025-04-13 23:42:14 +0100

    utils

---
+++ Ejercicio_1/utils.py
@@ -0,0 +1,5 @@
+def mostrar_pilas(origen, auxiliar, destino):
+    print("\nEstado actual de las pilas:")
+    print(f"Origen  : {origen}")
+    print(f"Auxiliar: {auxiliar}")
+    print(f"Destino : {destino}\n")
