e1a432a626513cc82c95c0f3ed47ecc5af17b61a
luis-lop-nas <luislopnas@icloud.com>
2025-04-13 23:40:47 +0100

    clase pila

---
+++ Ejercicio_1/pila.py
@@ -0,0 +1,22 @@
+class Pila:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop() if not self.is_empty() else None
+
+    def peek(self):
+        return self.items[-1] if not self.is_empty() else None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def __len__(self):
+        return len(self.items)
+
+    def __str__(self):
+        # Muestra la pila desde arriba hacia abajo
+        return str(self.items[::-1])
