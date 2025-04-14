d7b8920dc329ad373eb1a6acaace7a4761c3af0f
luis-lop-nas <luislopnas@icloud.com>
2025-04-14 16:55:25 +0100

    cambios en colores

--- Ejercicio_1/visual/colores.py
+++ Ejercicio_1/visual/colores.py
@@ -5,3 +5,10 @@ AZUL = (0, 102, 204)
 VERDE = (0, 153, 0)
 ROJO = (204, 0, 0)
 AMARILLO = (255, 204, 0)
+MORADO = (153, 0, 204)
+CELESTE = (0, 204, 204)
+
+_PALETA = [AZUL, VERDE, ROJO, AMARILLO, MORADO, CELESTE]
+
+def color_piedra(tamaño):
+    return _PALETA[(tamaño - 1) % len(_PALETA)]
